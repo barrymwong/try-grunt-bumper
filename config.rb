9398dce83041ea70e3b2cd9ed612879c30d528d8
Barry Wong <barry@barrymwong.com>
2014-07-02 13:24:08 -0700

    Release v1.2.12

--- config.rb
+++ config.rb
@@ -18,7 +18,7 @@ Compass::Frameworks.register('gctheme', :path => extension_path)
 #	a prerelease version
 #	Date is in the form of YYYY-MM-DD
 module Gctheme
-	VERSION = "1.2.11"
+	VERSION = "1.2.12"
 	DATE = "2014-07-02"
 end
 
