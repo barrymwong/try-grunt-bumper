361dfc987755b09836b19d570c0877e8aed7bd05
Barry Wong <barry@barrymwong.com>
2014-07-02 12:20:32 -0700

    Release v1.0.5

--- config.rb
+++ config.rb
@@ -18,8 +18,8 @@ Compass::Frameworks.register('gctheme', :path => extension_path)
 #	a prerelease version
 #	Date is in the form of YYYY-MM-DD
 module Gctheme
-	VERSION = 1.0.3
-	DATE = 2014-07-02
+	VERSION = "1.0.5"
+	DATE = "2014-07-02"
 end
 
 # This is where any custom SassScript should be placed. The functions will be
